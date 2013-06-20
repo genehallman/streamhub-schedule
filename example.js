794d36f94400d8aaf27eba13b3fe576996f8a292
Gene Hallman <gene@livefyre.com>
2013-06-20 16:31:17 -0700

    styled example

--- example.js
+++ example.js
@@ -9,8 +9,8 @@ define(function(require) {
                     start_date: "2013-03-26T12:00:00.00",
                     end_date: "2013-03-26T14:00:00.00",
                     article_id: 2001,
-                    title: "My title1",
-                    description: "My description1"
+                    title: "Is Mobile Really a Branding Vehicle?",
+                    description: "Dennis Maloney - Dominos<br>\r\nRich Lesperance - Walgreens<br>\r\nTim Reis - Google<br>\r\n<br>\r\n<a href=\"http:\/\/schedule.sxsw.com\/2013\/events\/event_IAP16070\" target=\"_blank\">Visit sxsw.com<\/a>"
                 }
             },{
                 pk: 1002,
@@ -18,8 +18,8 @@ define(function(require) {
                     start_date: "2013-03-26T14:00:00.00",
                     end_date: "2013-03-26T16:00:00.00",
                     article_id: 2002,
-                    title: "My title2",
-                    description: "My description2"
+                    title: "What Marketers Should Ask Themselves About Social",
+                    description: "Jeffrey Dachis - Dachis Group\r\n<br>\r\n<br>\r\n<a href=\"http:\/\/schedule.sxsw.com\/2013\/events\/event_IAP16074\" target=\"_blank\">Visit sxsw.com<\/a>"
                 }
             },{
                 pk: 1003,
@@ -27,8 +27,8 @@ define(function(require) {
                     start_date: "2013-03-26T13:00:00.00",
                     end_date: "2013-03-26T15:00:00.00",
                     article_id: 2003,
-                    title: "My title3",
-                    description: "My description3"
+                    title: "Emerging Media & Devices",
+                    description: "Glenn Stansbury - Hipcricket<br>\r\nMaani Safa - Somo Global<br>\r\nNicholas Oliver - Team Detroit<br>\r\nThomas Fellger - Iconmobile\r\n<br>\r\n<br>\r\n<a href=\"http:\/\/schedule.sxsw.com\/2013\/events\/event_IAP15969\" target=\"_blank\">Visit sxsw.com<\/a>"
                 }
             },{
                 pk: 1004,
@@ -36,8 +36,8 @@ define(function(require) {
                     start_date: "2013-03-26T15:00:00.00",
                     end_date: "2013-03-26T17:00:00.00",
                     article_id: 2004,
-                    title: "My title4",
-                    description: "My description4"
+                    title: "Opening Remarks from Bre Pettis",
+                    description: "Bre Pettis - MakerBot Industries\r\n<br>\r\n<br>\r\n<a href=\"http:\/\/schedule.sxsw.com\/2013\/events\/event_IAP15973\" target= \"_blank\">Visit sxsw.com<\/a>"
                 }
             },{
                 pk: 1005,
@@ -45,8 +45,8 @@ define(function(require) {
                     start_date: "2013-03-26T16:00:00.00",
                     end_date: "2013-03-26T18:00:00.00",
                     article_id: 2005,
-                    title: "My title5",
-                    description: "My description5"
+                    title: "Toward the Unification of Physics",
+                    description: "Steve Berger - Texas A&M\r\n<br>\r\n<br>\r\n<a href=\"http:\/\/schedule.sxsw.com\/2013\/events\/event_IAP16008\" target=\"_blank\">Visit sxsw.com<\/a>"
                 }
             },{
                 pk: 1006,
@@ -54,26 +54,26 @@ define(function(require) {
                     start_date: "2013-03-26T17:00:00.00",
                     end_date: "2013-03-26T19:00:00.00",
                     article_id: 2006,
-                    title: "My title6",
-                    description: "My description6"
+                    title: "Tales of US Entrepreneurship Beyond Silicon Valley",
+                    description: "Alexis Ohanian - Breadpig\r\n<br>\r\n<br>\r\n<a href=\"http:\/\/schedule.sxsw.com\/2013\/events\/event_IAP16012\" target=\"_blank\">Visit sxsw.com<\/a>"
                 }
             },{
                 pk: 1007,
                 fields: {
-                    start_date: "2013-03-26T14:00:00.00",
-                    end_date: "2013-03-26T15:00:00.00",
+                    start_date: "2013-03-26T15:45:00.00",
+                    end_date: "2013-03-26T18:30:00.00",
                     article_id: 2007,
-                    title: "My title7",
-                    description: "My description7"
+                    title: "The Rise of the Experience Economy",
+                    description: "Aileen Lee - Robot Cowboy Ventures<br>\r\nJennifer Hyman - Rent The Runway\r\n<br>\r\n<br>\r\n<a href=\"http:\/\/schedule.sxsw.com\/2013\/events\/event_IAP16030\" target=\"_blank\">Visit sxsw.com<\/a>"
                 }
             },{
                 pk: 1008,
                 fields: {
-                    start_date: "2013-03-26T16:00:00.00",
-                    end_date: "2013-03-26T17:30:00.00",
+                    start_date: "2013-03-26T13:00:00.00",
+                    end_date: "2013-03-26T14:30:00.00",
                     article_id: 2008,
-                    title: "My title8",
-                    description: "My description8"
+                    title: "Enterprise Invades Apps",
+                    description: "Eric Lai - Avaya<br>\r\nJohn Arrow - Mutual Mobile<br>\r\nMatt Carrier - SAP\r\n<br>\r\n<br>\r\n<a href=\"http:\/\/schedule.sxsw.com\/2013\/events\/event_IAP438\">Visit sxsw.com<\/a>"
                 }
             }
         ];
